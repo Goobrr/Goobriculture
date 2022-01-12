841fe81bcdc1808f613ec9f982f28ac3ecde1782
sample-text-here <56274831+sample-text-here@users.noreply.github.com>
2022-01-12 01:44:56 -0800

    combustible lemons

--- src/agriculture/content/PlantItems.java
+++ src/agriculture/content/PlantItems.java
@@ -5,7 +5,8 @@ import mindustry.type.*;
 public class PlantItems{
     public static Item
 
-    moyuzoFruit = new Item("moyuzo-berries"){
-
-    };
+    moyuzoFruit = new Item("moyuzo-berries"){{
+		flammability = 3.3f;
+		explosiveness = 1.1f;
+    }};
 }
